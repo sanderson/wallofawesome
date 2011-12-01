ccd9055c81f4ef3fa666b68ffad7893db3812b24
Scott Anderson <sanderson@delorum.com>
2011-12-01 16:49:56 -0700

    removed bar from page title

--- wp-content/themes/imbalance2/header.php
+++ wp-content/themes/imbalance2/header.php
@@ -7,7 +7,7 @@
 	wp_title( '|', true, 'right' );
 	bloginfo( 'name' );
 	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description";
+	if ( $site_description && ( is_home() || is_front_page() ) ) echo "  $site_description";
 	if ( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', 'imbalance2' ), max( $paged, $page ) );
 ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
