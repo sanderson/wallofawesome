911abd667c6b2fcf4c526898b887a410188ec822
Scott Anderson <sanderson@delorum.com>
2011-12-01 16:52:42 -0700

    tried to remove bar again

--- wp-content/themes/imbalance2/header.php
+++ wp-content/themes/imbalance2/header.php
@@ -4,10 +4,10 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php
 	global $page, $paged;
-	wp_title( '|', true, 'right' );
+	wp_title( '', true, 'right' );
 	bloginfo( 'name' );
 	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) ) echo "  $site_description";
+	if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description";
 	if ( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', 'imbalance2' ), max( $paged, $page ) );
 ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
