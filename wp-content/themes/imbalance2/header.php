68275701a4a5b86227a933a272d18f3c836945a3
Scott Anderson <sanderson@delorum.com>
2011-12-01 17:00:29 -0700

    tried to fix teh bar again

--- wp-content/themes/imbalance2/header.php
+++ wp-content/themes/imbalance2/header.php
@@ -4,11 +4,11 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php
 	global $page, $paged;
-	wp_title( '', true, 'right' );
+	wp_title( '|', true, 'right' );
 	bloginfo( 'name' );
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description";
-	if ( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', 'imbalance2' ), max( $paged, $page ) );
+	if ( $paged >= 2 || $page >= 2 ) echo '  ' . sprintf( __( 'Page %s', 'imbalance2' ), max( $paged, $page ) );
 ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
