7e648a5ea1ed840387cce146e05cc20f57a16ced
Scott Anderson <sanderson@delorum.com>
2011-12-02 12:55:43 -0700

    added social icons to sprite

--- wp-content/themes/imbalance2/footer.php
+++ wp-content/themes/imbalance2/footer.php
@@ -4,7 +4,7 @@
 		<div id="site-info">
 			© 2011 <a href="http://www.pagodabox.com/">Pagoda Box</a>
 		</div><!-- #site-info -->
-		<div id="footer-right"><?php wp_nav_menu( array( 'container_class' => 'menu', 'theme_location' => 'footer-right', 'walker' => new Imbalance2_Walker_Nav_Menu(), 'depth' => 1 ) ); ?></div>
+		<div id="footer-right"><a href="http://www.pagodabox.com"><img src="http://wallofawesome.pagodabox.com/wp-content/uploads/2011/12/pagodafloat.png"></a></div>
 		<div id="footer-left"><?php wp_nav_menu( array( 'container_class' => 'menu', 'theme_location' => 'footer-left', 'walker' => new Imbalance2_Walker_Nav_Menu(), 'depth' => 1 ) ); ?></div>
 		<div class="clear"></div>
 	</div><!-- #footer -->
